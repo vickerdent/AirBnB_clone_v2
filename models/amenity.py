56490a9713854ddd2b27dcbda6309e5b674a7eaa
Victor Abuka <vickerdent@gmail.com>
2023-09-21 03:48:42 +0100

    upload tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+'''
+    Implementation of the Amenity class
+'''
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    '''
+        Implementation for the Amenities.
+    '''
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
